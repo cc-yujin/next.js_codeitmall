a7839c090650edc5a1adc89b489751e39b4fe96c
HANYUJIN <mooo7570@gmail.com>
2023-11-09 16:29:53 +0900

    feat: useRouter

--- pages/search.js
+++ pages/search.js
@@ -1,3 +1,13 @@
+import { useRouter } from "next/router";
+
 export default function Search() {
-  return <div>Search 페이지</div>;
+  const router = useRouter(); // router 객체 만들기
+  const { q } = router.query; // router.query 객체에서 q 라는 이름으로 가져오겠다.
+
+  return (
+    <div>
+      <h1>Search 페이지</h1>
+      <h2>{q} 검색 결과</h2>
+    </div>
+  );
 }
