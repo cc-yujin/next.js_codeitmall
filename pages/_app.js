7cd533aa86803dd92ed1cb25dc490134cecc1511
HANYUJIN <mooo7570@gmail.com>
2024-01-22 21:20:03 +0900

    App 커스텀

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/global.css';
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react';
-import ProductList from '@/components/ProductList';
-import SearchForm from '@/components/SearchForm';
-import axios from '@/lib/axios';
-import styles from '@/styles/Home.module.css';
-import Header from '@/components/Header';
-import Container from '@/components/Container';
+import { useEffect, useState } from "react";
+import ProductList from "@/components/ProductList";
+import SearchForm from "@/components/SearchForm";
+import axios from "@/lib/axios";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
-    const res = await axios.get('/products');
+    const res = await axios.get("/products");
     const nextProducts = res.data.results;
     setProducts(nextProducts);
   }
@@ -21,11 +19,8 @@ export default function Home() {
 
   return (
     <>
-      <Header />
-      <Container>
-        <SearchForm />
-        <ProductList className={styles.products} products={products} />
-      </Container>
+      <SearchForm />
+      <ProductList className={styles.products} products={products} />
     </>
-  )
+  );
 }
