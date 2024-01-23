df15b041484da05bf2cfc6c7919026050cb38a92
HANYUJIN <mooo7570@gmail.com>
2024-01-23 16:11:43 +0900

    Head 컴포넌트 적용

--- pages/search.js
+++ pages/search.js
@@ -4,8 +4,7 @@ import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import axios from "@/lib/axios";
 import styles from "@/styles/Search.module.css";
-import Header from "@/components/Header";
-import Container from "@/components/Container";
+import Head from "next/head";
 
 export default function Search() {
   const [products, setProducts] = useState([]);
@@ -23,12 +22,15 @@ export default function Search() {
   }, [q]);
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>{q} 검색 결과 - Codeitmall</title>
+      </Head>
       <SearchForm initialValue={q} />
       <h2 className={styles.title}>
         <span className={styles.keyword}>{q}</span> 검색 결과
       </h2>
       <ProductList className={styles.productList} products={products} />
-    </div>
+    </>
   );
 }
