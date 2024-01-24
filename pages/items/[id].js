be3c2d10b5999dbc1aa5346b4171ec2141ea1c0a
HANYUJIN <mooo7570@gmail.com>
2024-01-24 16:09:01 +0900

    리뷰 작성

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import Image from "next/image";
 import axios from "@/lib/axios";
 import styles from "@/styles/Product.module.css";
 import SizeReviewList from "@/components/SizeReviewList";
 import StarRating from "@/components/StarRating";
-import Image from "next/image";
 import Spinner from "@/components/Spinner";
-import { useState } from "react";
+import Dropdown from "@/components/Dropdown";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import sizeReviewLabels from "@/lib/sizeReviewLabels";
 
 export async function getServerSideProps(context) {
   const productId = context.params["id"];
@@ -44,12 +48,17 @@ export default function Product({ product, sizeReviews: initialSizeReviews }) {
       ...formValue,
       productId: product.id,
     };
-    const res = await axios.post("/size_reviews", sizeReview);
+    const res = await axios.post("/size_reviews/", sizeReview);
     const newSizeReview = res.data;
-    setSizeReviews((prevSizeReview) => [newSizeReview, ...prevSizeReview]);
+    setSizeReviews((prevSizeReviews) => [newSizeReview, ...prevSizeReviews]);
   }
 
-  function handleChange(name, value) {
+  async function handleInputChange(e) {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  }
+
+  async function handleChange(name, value) {
     setFormValue({
       ...formValue,
       [name]: value,
@@ -125,6 +134,63 @@ export default function Product({ product, sizeReviews: initialSizeReviews }) {
           </section>
           <section className={styles.section}>
             <h2 className={styles.sectionTitle}>사이즈 추천하기</h2>
+            <form className={styles.sizeForm} onSubmit={handleSubmit}>
+              <label className={styles.label}>
+                사이즈
+                <Dropdown
+                  className={styles.input}
+                  name="size"
+                  value={formValue.size}
+                  options={[
+                    { label: "S", value: "S" },
+                    { label: "M", value: "M" },
+                    { label: "L", value: "L" },
+                    { label: "XL", value: "XL" },
+                  ]}
+                  onChange={handleChange}
+                />
+              </label>
+              <label className={styles.label}>
+                성별
+                <Dropdown
+                  className={styles.input}
+                  name="sex"
+                  value={formValue.sex}
+                  onChange={handleChange}
+                  options={[
+                    { label: sizeReviewLabels.sex["male"], value: "male" },
+                    { label: sizeReviewLabels.sex["female"], value: "female" },
+                  ]}
+                />
+              </label>
+              <label className={styles.label}>
+                키
+                <Input
+                  className={styles.input}
+                  name="height"
+                  min="50"
+                  max="200"
+                  type="number"
+                  value={formValue.height}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <label className={styles.label}>
+                사이즈 추천
+                <Dropdown
+                  className={styles.input}
+                  name="fit"
+                  value={formValue.fit}
+                  options={[
+                    { label: sizeReviewLabels.fit["small"], value: "small" },
+                    { label: sizeReviewLabels.fit["good"], value: "good" },
+                    { label: sizeReviewLabels.fit["big"], value: "big" },
+                  ]}
+                  onChange={handleChange}
+                />
+              </label>
+              <Button className={styles.submit}>작성하기</Button>
+            </form>
           </section>
         </div>
       </div>
